e05b73781d3b3befa447850f287757bfea877077
UclanPig2017 <32983866+UclanPig2017@users.noreply.github.com>
2017-10-21 17:35:51 +0100

    Basic comments
    Testing documentation

--- testWinForms/testWinForms/Form1.cs
+++ testWinForms/testWinForms/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//Basic test program that takes input string from user, then generates output string
 namespace testWinForms
 {
     public partial class Form1 : Form
@@ -19,10 +20,12 @@ namespace testWinForms
 
         private void btnNameSubmit_Click(object sender, EventArgs e)
         {
+            //If textbox is empty, output:
             if (string.IsNullOrWhiteSpace(txtName.Text))
             {
                 txtHello.Text = "Who are you?";
             }
+            //If textbox has content, generate string using content as below:
             else
             {
                 txtHello.Text = "Hello " + txtName.Text + " !";
